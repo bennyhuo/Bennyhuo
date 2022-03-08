948c22a7640e2f7c6b3b319f890878a6e818bfa3
bennyhuo <bennyhuo@kotliner.cn>
2022-03-08 17:03:40 +0800

    update

--- scripts/push_book.sh
+++ scripts/push_book.sh
@@ -1,9 +1,22 @@
-WORKING_DIR=$(basename $(pwd))
-git init
-git checkout -b $WORKING_DIR
-git add .
-git commit -m 'update'
+WORKING_DIR=$(cd $(basename .) && pwd -P)
+BRANCH_NAME=$(basename $WORKING_DIR)
 
+TEMP_DIR_NAME=".deploy"
+mkdir $TEMP_DIR_NAME
+cd $TEMP_DIR_NAME
+
+git init
 git remote add origin https://github.com/bennyhuo/Books.git
-git push --force origin $WORKING_DIR
-git gc
+git fetch
+git checkout $BRANCH_NAME
+
+cp $WORKING_DIR/*.md ./
+
+if [ $(git status --porcelain | wc -l) -eq "0" ]; then
+    echo "No updates in $BRANCH_NAME."
+else
+    echo "Update $BRANCH_NAME ..."
+    git add .
+    git commit -m 'update'
+    git push origin $BRANCH_NAME
+fi
